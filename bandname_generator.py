d7ad15653d5cf51402b4d054772cef41379e867e
DataWeeb <rohitb3010@gmail.com>
2025-12-30 16:39:20 +0530

    day1 band name generator

---
+++ bandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name genrator")
+name=input("which is your favourite location \n")
+petname=input("what was the name of you pet \n")
+print("your band name is "+name+" "+petname)
